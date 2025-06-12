69999b71264f2728a8bc061c2614b3733b74ca73
Ari <arlenehdez0225@gmail.com>
2025-06-11 22:16:18 -0400

    Add files via upload

--- Project2/project2.py
+++ Project2/project2.py
@@ -15,7 +15,7 @@ st.info("Type a city name or ZIP code")
 dotenv.load_dotenv()
 api_key = os.getenv("WEATHER_API_KEY")
 
-url = (f"http://api.weatherstack.com/forecast?access_key={api_key}&query={location}")
+url = (f"http://api.weatherstack.com/current?access_key={api_key}&query={location}")
 response = requests.get(url).json()
 
 def write_json(events, new_data, filename = "json_files/planned_events.json"):
@@ -30,7 +30,7 @@ def convert_dates(obj):
         return obj.isoformat()
     raise TypeError("Object is not a date")
 
-tab1, tab2, tab3 = st.tabs(["Events", "Weather Forecast", "Map"])
+tab1, tab2, tab3 = st.tabs(["Events", "Weather", "Map"])
 
 with tab1:
     st.subheader("Plan Events")
@@ -56,89 +56,57 @@ with tab1:
 
     with(open("json_files/planned_events.json", "r")) as read_file:
         file_data = json.load(read_file)
-        if len(file_data["planned_events"]) > 0:
-            idx = []
-            for i in range (len(file_data["planned_events"])):
-                idx.append(str(i))
-                df = pd.DataFrame(file_data["planned_events"], index=idx)
-                st.dataframe(df)
+        if "planned_events" in file_data and len(file_data["planned_events"]) > 0:
+            df = pd.DataFrame(file_data["planned_events"])
+            st.dataframe(df)
+        else:
+            st.warning("No planned events found")
 
 
 with tab2:
-    st.subheader("Weather Forecast")
-    forecast_days = st.slider("How many days you want to see?", min_value=1, max_value=14, step=1)
-
-    hourly = st.checkbox("Would you forecast in hours?")
-    if hourly:
-        hours = st.selectbox("How many hours do you want to see?", options=[1, 3, 6, 12, 24])
-        querystring = {"forecast_days": forecast_days,"hours": hours}
-    else:
-        querystring = {"forecast_days": forecast_days}
-
-    response = requests.get(url, params=querystring)
+    st.subheader(f"Today's Weather in {location}")
 
     col1, col2 = st.columns([2,5])
     with col1:
-        parameter = st.selectbox("Choose a parameter",options=[
-            "Temperature",
-            "Humidity",
-            "Wind Speed",
-            "Chance of Rain",
-            "Chance of Snow"
-        ])
+        weather_desc = response["current"]["weather_descriptions"]
+        st.write(f"The weather is: {weather_desc}")
+        st.image(response["current"]["weather_icons"])
 
     with col2:
-        data = response.json()
-        forecast = data.get("forecast", {})
-        df_list = []
+        def bar_chart(data):
+            long_df = data.melt(var_name="Weather Condition", value_name="Value")
+            fig = px.bar(long_df, x="Weather Condition", y="Value", title=f"Current Weather", color="Weather Condition")
+            return fig
 
-        for day_key in list(forecast.keys())[:forecast_days]:
-            day_data = forecast[day_key]
-            date = day_data.get("date", day_key)
-            for hour_data in day_data.get("hourly", [])[:hours if hourly else 24]:
-                df_list.append({
-                    "Date": date,
-                    "Time": hour_data.get("time"),
-                    "Temperature": hour_data.get("temperature", None),
-                    "Humidity": hour_data.get("humidity", None),
-                    "Wind Speed": hour_data.get("wind_speed", None),
-                    "Chance of Rain": hour_data.get("chanceofrain", None),
-                    "Chance of Snow": hour_data.get("chanceofsnow", None)
-                })
+        data = pd.DataFrame({
+            "Temperature": [float(response["current"]["temperature"])],
+            "Humidity": [float(response["current"]["humidity"])],
+            "Wind Speed": [float(response["current"]["wind_speed"])],
+            "Wind Pressure": [float(response["current"]["pressure"])],
+            "Cloud Cover": [float(response["current"]["cloudcover"])]
+        })
+        weather_chart = bar_chart(data)
 
-        df = pd.DataFrame(df_list)
-        df["Time"] = df["Time"].astype(str).str.zfill(4)
-        df["DateTime"] = pd.to_datetime(df["Date"] + " " + df["Time"],format="%Y-%m-%d %H%M",errors="coerce")
-        df = df.dropna(subset=["DateTime", parameter])
-        df = df.sort_values("DateTime")
-
-        if not df.empty and "DateTime" in df.columns and parameter in df.columns:
-            forecast_chart = st.line_chart(
-                data = df,
-                x= "DateTime",
-                y= parameter
-            )
-        else:
-            st.warning("No data found")
-        st.plotly_chart(forecast_chart)
+        st.plotly_chart(weather_chart)
 
 
 with tab3:
-    st.subheader("Map")
+    st.subheader(f"Map of {location}")
 
-    df = pd.DataFrame({
+    zoom_in = st.slider("Zoom in", min_value= 0, max_value=20, value=11)
+    d = pd.DataFrame({
         'lat' : float(response["location"]["lat"]),
         'lon' : float(response["location"]["lon"]),
-        'location' :[location]
+        'location' :[location],
 
     })
-    location_map = px.scatter_mapbox(
-        data_frame= df,
+    location_map = px.scatter_map(
+        data_frame= d,
         lat = "lat",
         lon = "lon",
         hover_name="location",
-        zoom = 11,
-        mapbox_style= "open-street-map"
+        zoom = zoom_in,
+        map_style= "open-street-map"
     )
     st.plotly_chart(location_map)
 
